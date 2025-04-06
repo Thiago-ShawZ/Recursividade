abfada4435e17ae7bf2f45f7a5b011f4a2cc5e82
Thiago Costa <85132867+Thiago-ShawZ@users.noreply.github.com>
2025-04-06 18:21:27 -0300

    Questão 19 Atividade Thiago.cpp

--- Atividade Thiago.cpp
+++ Atividade Thiago.cpp
@@ -277,4 +277,21 @@ int main() {
     printf("%d\n", comprimentoString(texto));
     return 0;
 }
+//19
+#include <stdio.h>
 
+void torreHanoi(int n, char origem, char auxiliar, char destino) {
+    if (n == 1) {
+        printf("Mover disco 1 de %c para %c\n", origem, destino);
+        return;
+    }
+    torreHanoi(n - 1, origem, destino, auxiliar);
+    printf("Mover disco %d de %c para %c\n", n, origem, destino);
+    torreHanoi(n - 1, auxiliar, origem, destino);
+}
+
+int main() {
+    int n = 2;
+    torreHanoi(n, 'A', 'B', 'C');
+    return 0;
+}
