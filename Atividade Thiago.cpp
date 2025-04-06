2ba44bb6986e2cb1c032d2b5a3983413ce6b6dc4
Thiago Costa <85132867+Thiago-ShawZ@users.noreply.github.com>
2025-04-06 18:33:26 -0300

    Questão 23 Atividade Thiago.cpp

--- Atividade Thiago.cpp
+++ Atividade Thiago.cpp
@@ -390,3 +390,54 @@ int main() {
     gerar_subconjuntos(arr, n);
     return 0;
 }
+//23
+#include <stdio.h>
+
+void merge(int arr[], int esquerda, int meio, int direita) {
+    int n1 = meio - esquerda + 1;
+    int n2 = direita - meio;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[esquerda + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[meio + 1 + j];
+
+    int i = 0, j = 0, k = esquerda;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int esquerda, int direita) {
+    if (esquerda < direita) {
+        int meio = esquerda + (direita - esquerda) / 2;
+        mergeSort(arr, esquerda, meio);
+        mergeSort(arr, meio + 1, direita);
+        merge(arr, esquerda, meio, direita);
+    }
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 1};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, tamanho - 1);
+
+    for (int i = 0; i < tamanho; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
