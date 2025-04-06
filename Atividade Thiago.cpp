360056dd5ce167335f01b9421a8297aa75818ca0
Thiago Costa <85132867+Thiago-ShawZ@users.noreply.github.com>
2025-04-06 18:22:03 -0300

    Questão 20 Atividade Thiago.cpp

--- Atividade Thiago.cpp
+++ Atividade Thiago.cpp
@@ -295,3 +295,31 @@ int main() {
     torreHanoi(n, 'A', 'B', 'C');
     return 0;
 }
+//20
+#include <stdio.h>
+#include <string.h>
+
+void trocar(char *x, char *y) {
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permutacoes(char *str, int inicio, int fim) {
+    if (inicio == fim) {
+        printf("%s\n", str);
+        return;
+    }
+    for (int i = inicio; i <= fim; i++) {
+        trocar(&str[inicio], &str[i]);
+        permutacoes(str, inicio + 1, fim);
+        trocar(&str[inicio], &str[i]);
+    }
+}
+
+int main() {
+    char str[] = "abc";
+    permutacoes(str, 0, strlen(str) - 1);
+    return 0;
+}
+
